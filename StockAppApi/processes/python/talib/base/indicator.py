aeda7038de824ad7b71d4e80506d631957387a11
Palash Halder <mpalash.halder@gmail.com>
2023-01-21 20:11:47 +0200

    Remove talib server implementaion for now * Talib calls will be made via  a systems. * The system will provide the interface and clean the data to call talib

--- StockAppApi/processes/python/talib/base/indicator.py
+++ StockAppApi/processes/python/talib/base/indicator.py
@@ -5,61 +5,21 @@ from scipy.stats import linregress
 from StockAppApi.processes.python.talib.interface.indicator_if import IndicatorIf
 
 class Indicator(IndicatorIf):
-    def __init__(self, name: str, type:str, ticker: str,ohlc:pandas.DataFrame, parameter:dict) -> None:
+    def __init__(self, ohlc:pandas.DataFrame, parameter:dict,ticker:str, name="", type="") -> None:
         self.name = name
         self.type = type # indicatpr can also have sub-types
         self.ticker = ticker
         self.ohlc = ohlc
         self.parameter = parameter
-        self.supported_command = {
-            'latest': self._latest,
-            'slope': self._slope
-        }
-    
-    def execute_command(self, command:str, condition="")->dict:
-        try:
-            return self.supported_command[command](condition)
-        except Exception as e:
-            raise
-    
-    def _latest(self, condition:str):
-        """Get the latest n rows with indicator result
-
-        Args:
-            condition (str): an empty string 
-
-        Returns:
-            _type_: status and latest macd
-        """
-        self._do_analysis()
-        n = int(condition.split("=")[1])
-        return self._result(status=True, obj=self.ohlc.tail(n))
-
-    def _slope(self, condition:str) -> dict:
-        """find slope of indicator type
-
-        Args:
-            condition (str): last N instances
-
-        Returns:
-            dict: of status with slope data
-        """
-        self._do_analysis()
-        
-        if condition == "":
-            data = self.ohlc[self.type]    
-        else:
-            n = int(condition.split("=")[1])
-            data = self.ohlc.tail(n)[self.type]
-        data = data.dropna()
-        slope, intercept, _, _, _ = linregress(numpy.arange(0, data.shape[0], 1), data)
-        return self._result(status=True, obj={
-            'slope': slope,
-            'intercept': intercept
-        })
-    
-    def _do_analysis(self):
+ 
+    def _do_analysis(self, latest=True):
         pass
     
-    def _result(self, status: bool, obj: object) -> dict:
-        return {'name':self.name, 'status': status, 'result': obj}
+    def get_result_df(self, with_latest_minute=True) -> pandas.DataFrame:
+        """Get the result as pandas dataframe 
+
+        Returns:
+            dict: pandas dataframe with additional column for analysed data 
+        """
+        self._do_analysis(latest=with_latest_minute)
+        return self.ohlc
--- StockAppApi/processes/python/talib/src/macd.py
+++ StockAppApi/processes/python/talib/src/macd.py
@@ -15,43 +15,17 @@ class Macd(Indicator):
     Args:
         Indicator (_type_): _description_
     """
-    def __init__(self, name, type ,ticker, ohlc, parameter) -> None:
+    def __init__(self, ohlc, parameter, ticker:str, name="", type="") -> None:
         super().__init__(name=name, type=type, ticker=ticker, ohlc=ohlc, parameter=parameter)
         
-        # add additional command on top of base indicator supported commands
-        self.supported_command.update({
-            'where': self.__where
-        })
-        
-    def __where(self, condition:str) -> dict:
-        """Search for instances where the condition is satisfied
-
-        Args:
-            condition (str): condition which checks with the macd or macd signal
-            or macd histogram 
-
-        Returns:
-            dict: analysis status with pandas dataframe
-        """
-        self._do_analysis()
-        try:
-            if 'value' in condition:
-                condition = condition.replace("value", f'self.ohlc["{self.type}"]')
-                return self._result(status=True, obj=self.ohlc.loc[eval(condition)])
-            elif 'diff' in condition:
-                condition = condition.replace("diff", f'self.ohlc["{self.type}"].diff()')
-                return self._result(status=True, obj=self.ohlc.loc[eval(condition)])
-        except Exception as e:
-            raise
-        return self._result(status=False, obj=None)
-
-    def _do_analysis(self):
-        last_minute_series = download_latest_data(tickers=[f'{self.ticker}.NS'])
-        self.ohlc = pandas.concat([self.ohlc, last_minute_series])
+    def _do_analysis(self, latest=True):
+        if latest:
+            last_minute_series = download_latest_data(tickers=[f'{self.ticker}.NS'])
+            self.ohlc = pandas.concat([self.ohlc, last_minute_series])
         macd, macdsignal, macdhist = talib.MACD(self.ohlc[self.parameter['ohlc']], 
-                                                fastperiod=self.parameter['fastperiod'],
-                                                slowperiod=self.parameter['slowperiod'],
-                                                signalperiod=self.parameter['signalperiod'])
+                                                fastperiod=self.parameter['macd_fast_period'],
+                                                slowperiod=self.parameter['macd_slow_period'],
+                                                signalperiod=self.parameter['macd_signal_period'])
         macd = numpy.around(macd, decimals=2)
         macdsignal = numpy.around(macdsignal, decimals=2)
         macdhist = numpy.around(macdhist, decimals=2)
@@ -59,4 +33,4 @@ class Macd(Indicator):
         self.ohlc['macdsignal'] = macdsignal
         self.ohlc['macdhist'] = macdhist
         
-        return macd
+        return macd, macdsignal, macdhist
