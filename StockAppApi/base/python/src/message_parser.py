8ace0e31704ef5194442e1d33d5fe1f9079d4384
Palash Halder <mpalash.halder@gmail.com>
2023-01-22 10:32:12 +0200

    Revert back to message parser using key and split.
    * Need to understand pyparsing and maybe such complex features are not needed

--- StockAppApi/base/python/src/message_parser.py
+++ StockAppApi/base/python/src/message_parser.py
@@ -1,31 +1,26 @@
-from pyparsing import Word, alphanums, Group, Forward, ZeroOrMore, oneOf, Keyword, Suppress, OneOrMore
+# from pyparsing import *
+# alphanum = Word(alphanums + '<' + '>' + '<=' + '>=' + ',' + '_' + '-')
 
-alphanum = Word(alphanums + '<' + '>' + '<=' + '>=' + ',' + '_')
+# key_operator= Literal('--')
+# # key_operator = oneOf(key_symbol, caseless=True)
 
-key_symbol= "--"
-key_operator = oneOf(key_symbol, caseless=True)
-value_operator = oneOf("< > <= >= ,")
-bool_operator=oneOf("&& || !!")
-pipe_operator = oneOf("|")
+# # Define a forward declarations
+# key_value = Forward()
+# key_value <<= ZeroOrMore(key_operator + ZeroOrMore(alphanum))
 
-# Define a forward declarations
+# # Complete command query string
+# cmd_with_query = Forward()
+# cmd_with_query <<= Group(alphanum + key_value)
+from pyparsing import *
+word = Word(alphas + '<' + '>' + '<=' + '>=' + ',' + '_' + '-')
+dashed = Literal("--")
 key_value = Forward()
-key_value <<= ZeroOrMore(key_operator + ZeroOrMore(alphanum))
-
-# Main command comes before the pipe
-cmd = Forward()
-cmd <<= Group(alphanum + key_value)
-
-# sub-commands are joined and their result is computed 
-query = Forward()
-query <<= ZeroOrMore(cmd + ZeroOrMore(bool_operator))
-
-# Complete command query string
+key_value <<= ZeroOrMore(dashed + ZeroOrMore(word))
 cmd_with_query = Forward()
-cmd_with_query <<= cmd + ZeroOrMore(pipe_operator) + query
+cmd_with_query <<= Group(word + key_value)
 
-@DeprecationWarning
-def parse_message(message:str, parser_symbol:str) -> dict:
+# @DeprecationWarning
+def parse_message(message:str, parser_symbol='--') -> dict:
     try:
         messageToParse = message
         messageToParse.strip()
@@ -46,6 +41,7 @@ def parse_message(message:str, parser_symbol:str) -> dict:
     except Exception as e:
         raise
 
+'''
 def parse_message(message:str)->dict:
     """
     Parse the message string into key, value pairs. The string with the ``parser_symbol``
@@ -96,38 +92,42 @@ def parse_message(message:str)->dict:
     except Exception as e:
         print(e.args)
 
+'''
+
 # 
-def make_map(input: list)->dict:
-    """convert the list of commands to key-value pairs
+# def make_map(input: list)->dict:
+#     """convert the list of commands to key-value pairs
 
-    Args:
-        input (list): of parsed string
+#     Args:
+#         input (list): of parsed string
 
-    Returns:
-        dict: of key-value key are the identifiers of action
-    """
-    ret = {}
-    for i in range(len(input)):
-        if i == 0:            
-            if(isinstance(input, str)):
-                ret['command'] = input
-            else:
-                ret['command'] = input[i]
-        else:
-            if(input[i] == key_symbol):
-                ret[input[i+1]] = input[i+2]
-                i+=2
-    return ret
+#     Returns:
+#         dict: of key-value key are the identifiers of action
+#     """
+#     ret = {}
+#     for i in range(len(input)):
+#         if i == 0:            
+#             if(isinstance(input, str)):
+#                 ret['command'] = input
+#             else:
+#                 ret['command'] = input[i]
+#         else:
+#             if(input[i] == "--"):
+#                 ret[input[i+1]] = input[i+2]
+#                 i+=2
+#     return ret
         
 if __name__ == "__main__":
-    test = f'select --stock all --interval day | where --indicator ema --interval 20,100 \
-        --condition value<=1000 && where --indicator macd --interval 20,100 --condition slope>0 \
-        || where --indicator rsi --condition value>70'
-    test2 = f'select --stock all --interval day,week --condition helo>100'
-    test3 = "elder --ema_window 13 --ema_n 100 --macd_fast_period 13 macd_slow_period 26 macd_signal_period 9 --macdhist_n 20"
-    test4 ="sendMessage --channel general --message |    | stock      |   ema_day_slope |   ema_week_slope |   macd_hist_day_slope |   macd_hist_week_slope |   ema_action |   machdhist_action | trend   |\n|---:|:-----------|----------------:|-----------------:|----------------------:|-----------------------:|-------------:|-------------------:|:--------|\n|  1 | ASIANPAINT |        -3.59613 |         -7.20985 |              -1.56491 |                 -0.948 |           -2 |                 -2 | bearish |"
+    # test = f'select --stock all --interval day | where --indicator ema --interval 20,100 \
+    #     --condition value<=1000 && where --indicator macd --interval 20,100 --condition slope>0 \
+    #     || where --indicator rsi --condition value>70'
+    # test2 = f'select --stock all --interval day,week --condition helo>100'
+    test3 = "elder --ema_window 13 --ema_n 100 --macd_fast_period 13 --macd_slow_period 26 --macd_signal_period 9 --macdhist_n 20"
+    # test4 ="sendMessage --channel general --message |    | stock      |   ema_day_slope |   ema_week_slope |   macd_hist_day_slope |   macd_hist_week_slope |   ema_action |   machdhist_action | trend   |\n|---:|:-----------|----------------:|-----------------:|----------------------:|-----------------------:|-------------:|-------------------:|:--------|\n|  1 | ASIANPAINT |        -3.59613 |         -7.20985 |              -1.56491 |                 -0.948 |           -2 |                 -2 | bearish |"
+    test4 =f"talibquery --ticker BAJAJ-AUTO --interval day --do get --indicator  \
+        macdhist --fastperiod 13 --slowperiod 26 --signalperiod 9 --n 2"
     # print(parse_message(test))
     # print(parse_message(test2))
-    # print(parse_message(test3))
+    print(parse_message(test3))
     print(parse_message(test4))
     
