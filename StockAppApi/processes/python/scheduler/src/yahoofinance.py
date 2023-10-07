2a72a45f58334cec69a2dab9a152dfeaa963fd7d
atPalash <mpalash.halder@gmail.com>
2023-10-07 09:17:09 +0300

    Download weekly, monthly ohlc daily

--- StockAppApi/processes/python/scheduler/src/yahoofinance.py
+++ StockAppApi/processes/python/scheduler/src/yahoofinance.py
@@ -4,10 +4,11 @@ import pytz
 from StockAppApi.processes.python.scheduler.base.scheduler import Scheduler
 from StockAppApi.processes.python.system.src.command_handler import CommandHandler
 
+
 class YahooScheduler(Scheduler):
-    def __init__(self, indicator_config_file: str, selected_stocks_config_file:str, master_url:str) -> None:
-        super().__init__(indicator_config_file=indicator_config_file, 
-                         selected_stocks_config_file=selected_stocks_config_file, 
+    def __init__(self, indicator_config_file: str, selected_stocks_config_file: str, master_url: str) -> None:
+        super().__init__(indicator_config_file=indicator_config_file,
+                         selected_stocks_config_file=selected_stocks_config_file,
                          master_url=master_url)
         self.schedulers = {}
 
@@ -19,11 +20,11 @@ class YahooScheduler(Scheduler):
         for interval in self.indicator_config['indicator']['data']:
             self.__periodic_download(interval=interval)
             if interval == 'month':
-                scheduler.add_job(self.__periodic_download, 'cron', day='1', hour='0', minute='0',
-                                  timezone=pytz.timezone('Asia/Kolkata'), args=[interval])
+                scheduler.add_job(self.__periodic_download, 'cron',  hour='16',
+                                  day_of_week='mon-fri', timezone=pytz.timezone('Asia/Kolkata'), args=[interval])
             elif interval == 'week':
-                scheduler.add_job(self.__periodic_download, 'cron', hour='17',
-                                  day_of_week='fri', timezone=pytz.timezone('Asia/Kolkata'), args=[interval])
+                scheduler.add_job(self.__periodic_download, 'cron', hour='16',
+                                  day_of_week='mon-fri', timezone=pytz.timezone('Asia/Kolkata'), args=[interval])
             elif interval == 'day':
                 scheduler.add_job(self.__periodic_download, 'cron', hour='16',
                                   day_of_week='mon-fri', timezone=pytz.timezone('Asia/Kolkata'), args=[interval])
@@ -45,9 +46,9 @@ class YahooScheduler(Scheduler):
             else:
                 print(f"Error: This {interval} is not allowed")
             self.schedulers[interval] = scheduler
-        
-        scheduler.add_job(self.__weekly_fundamental_download, 'cron', day_of_week='fri', hour='23', 
-            timezone=pytz.timezone('Asia/Kolkata'))
+
+        scheduler.add_job(self.__weekly_fundamental_download, 'cron', day_of_week='fri', hour='23',
+                          timezone=pytz.timezone('Asia/Kolkata'))
         scheduler.start()
 
     def forceDownload(self, inter=""):
@@ -57,7 +58,7 @@ class YahooScheduler(Scheduler):
         else:
             self.__periodic_download(interval=inter)
         self.__weekly_fundamental_download()
-            
+
     def __periodic_download(self, interval: str):
         """Analysis method will include all the possible combination of the indicators.
         Donot call this method directly to avoid confusion, use the above methods. 
@@ -68,26 +69,33 @@ class YahooScheduler(Scheduler):
         """
         try:
             query = f"yahoofinance --ticker all --interval {interval} --do get --pandas 0  --csv 1"
-            ret = self.system_command_handler.execute(message=query, is_rest=False) # just download the data, only print the errors
+            # just download the data, only print the errors
+            ret = self.system_command_handler.execute(
+                message=query, is_rest=False)
             if ret.errors != "":
                 print("ERROR yahoo system", ret.errors)
         except Exception as e:
             print("ERROR __periodic_download", e.args)
-        
+
     def __weekly_fundamental_download(self):
         try:
             query = f"yahoofinance --ticker all --do fundamentals --pandas 0  --csv 1"
-            ret = self.system_command_handler.execute(message=query, is_rest=False) # just download the data, only print the errors
+            # just download the data, only print the errors
+            ret = self.system_command_handler.execute(
+                message=query, is_rest=False)
             if ret.errors != "":
-                print("ERROR yahoo fundamental download system", ret.errors) # TODO
+                print("ERROR yahoo fundamental download system",
+                      ret.errors)  # TODO
         except Exception as e:
             print("ERROR __weekly_fundamental_download", e.args)
+
 
 if __name__ == "__main__":
     from StockAppApi.base.python.src.yaml_parser import read_config
     configFolder = "StockAppApi/configuration/"
     config = read_config(configFolder + "config.yaml")
-    indicator_config_yaml = configFolder + "indicator.yaml"   
+    indicator_config_yaml = configFolder + "indicator.yaml"
     selected_stocks_yaml = configFolder + "selected_stocks.yaml"
-    scheduler = YahooScheduler(indicator_config_yaml, selected_stocks_yaml, "dummy")
+    scheduler = YahooScheduler(
+        indicator_config_yaml, selected_stocks_yaml, "dummy")
     scheduler.forceDownload("month")
