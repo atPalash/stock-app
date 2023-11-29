acd762d62950d0a837a10bb06a08a3876269fc23
palashTUT <mpalash.halder@gmail.com>
2023-11-29 17:05:29 +0000

    Add then step
    * Now then step will just show the result of last when step. The filter
    will be done in the when steps itself by using pipe type.

--- stock_app_py/system/src/steps/then/aggregator.py
+++ stock_app_py/system/src/steps/then/aggregator.py
@@ -3,6 +3,6 @@ from stock_app_py.system.src.steps.then import color, list_stocks, signals
 
 def get_steps():
     return {
-        r"^get list of (\w+) match$": list_stocks.get_list,
+        r"^get list$": list_stocks.get_list,
         r"^get list of stocks with signals$": signals.list_stock_signals,
     }
--- stock_app_py/system/src/steps/then/then.py
+++ stock_app_py/system/src/steps/then/then.py
@@ -1,5 +1,20 @@
-from stock_app_py.system.src.steps.then import aggregator
+from stock_app_py.system.src.steps import common
 
 
-def get():
-    pass
+def execute(
+    matched_step: dict,
+    all_step_result: list,
+    pipe_type: common.PipeType,
+) -> common.StepRet:
+    func_ret = matched_step["func"](all_step_result)
+
+    curr_step_ret = common.StepRet(
+        data=func_ret.get("signals", None),
+        pipe_tickers=func_ret["tickers"],
+        pipe_type=pipe_type,
+        err="",
+    )
+
+    return common.make_return(
+        prev_step_result=all_step_result[-1], curr_step_ret=curr_step_ret
+    )
