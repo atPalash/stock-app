7659c0ae8e8eb88c53b68850a080a5dd73376e10
Palash Halder <mpalash.halder@gmail.com>
2024-02-18 18:26:09 +0000

    Clean and remove redundant and unused files
    Signed-off-by: Palash Halder <mpalash.halder@gmail.com>

--- Dockerfiles/autocomplete.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-_init_completions()
-{
-  local cur=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=( $(compgen -W "setEnv setPythonPackages makeDirs" -- $cur) )
-}
-
-complete -F _init_completions ./init.bash
