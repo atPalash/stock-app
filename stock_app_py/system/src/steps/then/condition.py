4f6cc5bff6dbd26af6e5f920e6bda43a5a8f1b20
Palash Halder <mpalash.halder@gmail.com>
2024-03-03 16:27:16 +0000

    Then step to compute user condition
    Signed-off-by: Palash Halder <mpalash.halder@gmail.com>

---
+++ stock_app_py/system/src/steps/then/condition.py
@@ -0,0 +1,16 @@
+import pandas
+from stock_app_py.utility.src.steps import then
+from stock_app_py.system.src.steps import common
+
+
+@then
+def calculate(groups, query_df: pandas.DataFrame) -> pandas.DataFrame:
+    key, logic = groups
+    query_df[key] = False
+    for index, row in query_df.iterrows():
+        try:
+            # Assume eval_expr is a string representing an evaluatable expression
+            query_df.at[index, f'{key}'] = eval(logic, None, row.to_dict())
+        except Exception as e:
+            query_df.at[index, 'error'] = query_df.at[index, "error"] + f"then.calculate:{e.args}"
+    return query_df
