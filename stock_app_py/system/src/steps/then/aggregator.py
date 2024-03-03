0ea4c6ffe2bede4333d8e6e6ac91dabfe937ebc4
Palash Halder <mpalash.halder@gmail.com>
2024-03-03 16:23:44 +0000

    Implement mathmatical statement ability after =
    Signed-off-by: Palash Halder <mpalash.halder@gmail.com>

--- stock_app_py/system/src/steps/then/aggregator.py
+++ stock_app_py/system/src/steps/then/aggregator.py
@@ -1,6 +1,6 @@
 import stock_app_py.system.src.steps.common as comm
 from stock_app_py.system.src.steps.common import StepData
-from stock_app_py.system.src.steps.then import color, list_stocks, signals
+from stock_app_py.system.src.steps.then import color, list_stocks, signals, condition
 
 
 def get_steps():
@@ -14,16 +14,33 @@ def get_steps():
             variables={-1: StepData.empty},
         ),
         # get tickers with <logic>
-        r"^get tickers with ([^.?!]*[+\-*><!=/%][^.?!]*)$": StepData(
+        r"^get tickers with (.+)$": StepData(
             logic=list_stocks.calculate,
             variables={3: StepData.word},
+        ),
+
+        r"^let (\w+) = (.+)$": StepData(
+            logic=condition.calculate,
+            variables={
+                1: StepData.word,
+                3: StepData.word,
+            },
         ),
     }
 
 if __name__ == "__main__":
-    query = "get tickers with ema10 * ema50 - ema30 <= ema20 / ma50 - ema10"
+    query = "let close_ma150 = close > ma150"
+    query = "get tickers with close_ma50 & close_ma150 & close_ma200 & ma50_ma150 & ma50_ma200 & ma150_ma200 & uptrend200 & close_52wklow & close_52wkhigh"
     # query = "get list of stocks with signals"
     res = comm.get_matched_step(query, get_steps())
     print(res['match'].groups())
 
 # ([^.?!]*[+\-*></%][^.?!]*)
+        # #  let ma150 = <logic>
+        # r"^let (\w+) = ([^.?!]*[+\-*><!=/%][^.?!]*)$": StepData(
+        #     logic=condition.calculate,
+        #     variables={
+        #         1: StepData.word,
+        #         3: StepData.word,
+        #     },
+        # ),
