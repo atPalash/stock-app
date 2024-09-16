0dea5f74da6b3802bdade9005eadf212dbeb8447
atPalash <mpalash.halder@gmail.com>
2024-09-16 19:35:17 +0000

    Fix runners and user config to be used in rpi

--- runner.sh
+++ runner.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
 # Make dirs to store ohlc and fundamentals data. Note fundamentals is empty for now.
-mkdir database && cd database
-mkdir fundamentals ohlc
-cd ohlc && mkdir day hour minute minute5 minute15 minute30 month week
+# mkdir database && cd database
+# mkdir fundamentals ohlc
+# cd ohlc && mkdir day hour minute minute5 minute15 minute30 month week
+CONTAINER_NAME="stock-server"
+REMOTE_USER="palash"
+REMOTE_HOST="palashserver"
 
-cd ~/app
-
-# Start webserver at 8087
-python stock_app_py/webserver/main.py &
-P1=$!
-
-# Start scheduler at 8085
-python stock_app_py/scheduler/main.py &
-P2=$!
-
-# Start ngrok -> 8087, this will tunnel the domain to localhost
-ngrok http 8087 --domain=tight-strongly-liger.ngrok-free.app &
-P3=$!
-wait $P1 $P2 $P3
+# Open three separate terminals and execute the Python file
+gnome-terminal -- bash -c "ssh ${REMOTE_USER}@${REMOTE_HOST} 'podman exec -it ${CONTAINER_NAME} ngrok http --domain=tight-strongly-liger.ngrok-free.app 8087; echo \"ngrok started\"; exec bash'"
+gnome-terminal -- bash -c "ssh ${REMOTE_USER}@${REMOTE_HOST} 'podman exec -it ${CONTAINER_NAME} python ~/app/stock_app_py/scheduler/main.py; exec bash'"
+gnome-terminal -- bash -c "ssh ${REMOTE_USER}@${REMOTE_HOST} 'podman exec -it ${CONTAINER_NAME} python ~/app/stock_app_py/webserver/main.py; exec bash'"
