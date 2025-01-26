020d3ecac909159b112ebff07e72b8719732daad
Palash Halder <mpalash.halder@gmail.com>
2025-01-26 19:55:32 +0000

    Helper to calculate remaining days

--- stock_app_py/utility/src/date_helper.py
+++ stock_app_py/utility/src/date_helper.py
@@ -1,4 +1,5 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
+
 
 def find_closest_date(target_date, date_list):
     closest_date = None
@@ -11,3 +12,17 @@ def find_closest_date(target_date, date_list):
             closest_date = date_list[i]
             index = i
     return closest_date, index
+
+
+def days_until(start_date: str, target_date: str, date_format="%d-%b-%Y"):
+    # Parse the date string into a datetime object
+    if start_date == "today":
+        start_date = datetime.now()
+    else:
+        start_date = datetime.strptime(start_date, date_format)
+    target_date = datetime.strptime(target_date, date_format)
+
+    # Calculate the difference in days
+    remaining_days = (target_date - start_date).days
+
+    return remaining_days
